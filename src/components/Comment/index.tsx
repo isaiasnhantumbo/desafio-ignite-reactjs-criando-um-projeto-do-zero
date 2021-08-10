1fb3d0ece45d125f723bbc04b552f6bb824cce90
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-08-10 17:21:44 +0200

    :recycle: refactor: Added Comment Component

---
+++ src/components/Comment/index.tsx
@@ -0,0 +1,10 @@
+import { useUtterances } from '../../hooks/useUtterances';
+
+const commentNodeId = 'comments';
+
+const Comments = () => {
+  useUtterances(commentNodeId);
+  return <div id={commentNodeId} />;
+};
+
+export default Comments;
